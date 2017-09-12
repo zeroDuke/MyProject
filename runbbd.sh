818b22dbb74840eec606558cbbe0064db3962a86
zeroDuke <zhaoyao8727@yeah.net>
2017-09-12 13:09:38 +0800

    fix

--- runbbd.sh
+++ runbbd.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 filename=''
-netmask='255.255.255.0'
-gateway='192.168.255.2'
-prefix='CUC-SH-PD1'
-ip='192.168.255.11'
+netmask=''
+gateway=''
+prefix='CUC-SD-DZ1'
+ip=''
 getInfo(){
-  #pass=$(sudo ssh -p23432 -t root@$ip "hostname"|awk -F- '{ print $3 }')
-  #serial=$(sudo ssh -p23432 -t root@$ip "dmidecode|grep -m 1 'Serial Number:'"|awk -F '{ print $3 }')
-  pass=$(sudo ssh -p23432 -t $ip "hostname"|awk -F- '{ print $3 }')
+  pass=$(sudo ssh -p23432 -t root@$ip "hostname"|awk -F- '{ print $3 }')
+  serial=$(sudo ssh -p23432  root@$ip "dmidecode|grep -m 1 'Serial Number:'"|awk -F' ' '{ print $3 }')
+  #pass=$(sudo ssh -p23432 -t $ip "hostname"|awk -F- '{ print $3 }')
   #serial=$(sudo ssh -p23432 -t $ip "dmidecode|grep -m 1 'Serial Number:'")
-  serial="91732240312345"
+  #serial="91732240312345"
   echo -e $ip    $serial    $pass
 }
 
@@ -21,18 +21,18 @@ upLoad(){
 runInit(){
   newname=${prefix,,}-${ip//./-}
   echo $newname
-  sudo ssh -p23432 -t root@$ip "sh /tmp/bbd.bin --HOSTNAME=$newname | tee -a $ip.log ;sh /tmp/bbd.bin --basic sh | tee -a $ip.log ;sh /tmp/bbd.bin --basic sh | tee -a $ip.log "
+  sudo ssh -p23432 -t root@$ip "sh /tmp/bbd.bin --HOSTNAME=$newname | tee -a /tmp/$ip.log ;sh /tmp/bbd.bin --basic sh | tee -a /tmp/$ip.log ;sh /tmp/bbd.bin --basic sh | tee -a /tmp/$ip.log "
 }
 
 downLoadlog(){
   mkdir -p remotelog
-  sudo scp -p 23432 $ip:/tmp/$ip.log ./remotelog/$prefix/
+  sudo scp -p 23432 $ip:/tmp/$ip.log ./logs/remote/$prefix/
 }
 
-mkdir -p locallog
+mkdir -p logs/{local,remote}
 for ip in `cat iplist.txt` do
-  getInfo | tee -a ./locallog/$ip.log
+  getInfo  | tee -a logs/local/$ip.log
   upLoad
-  runInit | tee -a ./locallog/$ip.log
+  runInit | tee -a logs/local/$ip.log
   downLoadlog
 done
